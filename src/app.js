dda9308a6cc6a92a48febb760baa21cdb90761e1
Adithya Prasad <adithyasridharprasad@gmail.com>
2024-06-17 22:00:47 +0530

    update: cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import passport from "passport";
+import cors from "cors";
 import connectMongoDB from "./config/db/mongoDB.js";
 import sessionConfig from "./config/session/index.js";
 import routes from "./routes/index.js";
@@ -14,6 +15,15 @@ connectMongoDB();
 
 app.use(morganMiddleware);
 app.use(express.json());
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.PRODUCTION_URL
+        : `http://localhost:${process.env.PORT || 3000}`,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session(sessionConfig()));
 app.use(passport.initialize());
