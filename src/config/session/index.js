fa6337b198bd06191cec19ccb850874efbcd7c61
Adithya Prasad <adithyasridharprasad@gmail.com>
2024-06-18 08:43:58 +0530

    fix. session config

--- src/config/session/index.js
+++ src/config/session/index.js
@@ -10,7 +10,7 @@ function sessionConfig() {
     resave: false,
     cookie: {
       maxAge: parseInt(process.env.SESSION_MAX_AGE),
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
     },
     store: new MongoStore({
       client: mongoose.connection.getClient(),
